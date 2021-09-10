beae728dad0bcbea3b81091da8ed46944105cc36
Justaus3r <72691864+Justaus3r@users.noreply.github.com>
2021-09-10 21:56:52 +0500

    Fix[player_check]:Fix yet another Typo

--- player_check.bat
+++ player_check.bat
@@ -1,7 +1,5 @@
 @echo off
 setlocal enabledelayedexpansion
 set player_name=%~1
-
 %player_name% >nul 2>&1
-if %errorlevel% neq 0 (echo 1 >player_bool_file && exit) else (echo 0 >player_bool_file && exit)
-
+if %errorlevel% neq 0 (echo 0 >player_bool_file && exit) else (echo 1 >player_bool_file && exit)
