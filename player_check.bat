67ebaa87daf3b6f5b8fd1aeee7aa98ce1606ffb6
RaitaroHikami <19552720+RaitaroH@users.noreply.github.com>
2021-09-10 07:01:59 +0000

    bat to check any player

--- player_check.bat
+++ player_check.bat
@@ -1,12 +1,14 @@
 @echo off
 setlocal enabledelayedexpansion
-mpv >nul 2>&1
+set player_name=%~1
+
+where /q %player_name%
 if %errorlevel% neq 0 (call :player_notpresent) else (call :player_present)
 :player_present
 rem Present
-echo 1 >player_bool
+echo 1 >player_bool_file
 exit
 :player_notpresent
 rem Present
-echo 0 >player_bool
+echo 0 >player_bool_file
 exit
